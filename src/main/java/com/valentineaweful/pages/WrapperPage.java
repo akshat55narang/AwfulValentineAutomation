b11ed21897c08bbce9808ae059b280a584bab0cd
Akshat <akshat55narang@gmail.com>
2018-09-10 23:08:11 +0200

    implemented Generic Step Definitioions and Wrapper class for generic web driver methods

--- src/main/java/com/valentineaweful/pages/WrapperPage.java
+++ src/main/java/com/valentineaweful/pages/WrapperPage.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
+
 public class WrapperPage extends FleuentialWaitProvider {
     private WebDriver driver;
 
